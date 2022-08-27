b70e1887d3c6fcd2615e9af5016101afd2d00ec9
anne <annemachinda@gmail.com>
2022-08-27 02:56:28 +0100

    css and assets

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+      },
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+      },
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                },
+              },
+            ],
+          },
+        ],
+      },
+};
