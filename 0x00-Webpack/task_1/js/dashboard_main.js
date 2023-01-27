7e83b4427bd0a50f5b56750873aad884b7b7ad9d
anne <annemachinda@gmail.com>
2023-01-27 08:18:43 +0100

    require

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
-import $ from "jquery";
+const $ = require('jquery');
+const _ = require('lodash')
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -6,7 +7,7 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p> Copyright - Holberton School</p>');
 
-counter = 0;
+let counter = 0;
 function updateCounter() {
     counter = counter + 1;
     $('#count').html(`$(counter) clicks on the button`);
