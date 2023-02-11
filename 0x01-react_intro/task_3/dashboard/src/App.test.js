5fb1759ebc9a9b5220092ea09b12aa8043aa6dc9
anne <annemachinda@gmail.com>
2023-02-11 02:57:39 +0100

    test

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from 'enzyme';
 import App from './App';
 
-describe('App Components', () => {
+describe('App Component', () => {
     it('renders without crashing', () => {
         const app = shallow(<App />);
         expect(app).toBeDefined();
