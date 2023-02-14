dbec8b7edf7e54bebe00341a6df70cc48d2bc106
anne <annemachinda@gmail.com>
2023-02-14 13:28:05 +0100

    corrected alignment

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -8,12 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> School dashboard</h1>
-        <body> 
-          <p>Login to access the full dashboard</p>
+        <body className="App-body"> 
+          <p>Login to access the full dashboard</p><br></br><br></br> <br></br>
           <label htmlFor="email">Email: </label> 
-          <input name="email" type="email" id="email"></input> <br></br>
+          <input name="email" type="email" id="email"></input>
           <label htmlFor="password">Password: </label>
-          <input name="password" type="password" id="password"></input><br></br>
+          <input name="password" type="password" id="password"></input>
           <button>OK</button>
         
         </body>
