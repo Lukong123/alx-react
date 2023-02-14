788ea8d1e98e42dda547132670f95fc68339bdca
anne <annemachinda@gmail.com>
2023-02-14 13:45:12 +0100

    new config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,47 +1,94 @@
-const path = require('path');
+// const path = require('path');
+
+// module.exports = {
+//     mode: 'production',
+//     entry: {
+//         main: path.resolve(__dirname, './js/dashboard_main.js'),
+//       },
+//       output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist')
+//       },
+//       devServer: {
+//         hot: true,
+//         contentBase: path.resolve("./dist"),
+//         compress: true,
+//         port: 3000,
+//       },
+
+//       module: {
+//         rules: [
+//           {
+//             test: /\.css$/i,
+//             use: ["style-loader", "css-loader"],
+//           },
+//           {
+//             test: /\.(gif|png|jpe?g|svg)$/i,
+//             use: [
+//               'file-loader',
+//               {
+//                 loader: 'image-webpack-loader',
+//                 options: {
+//                   bypassOnDebug: true, // webpack@1.x
+//                   disable: true, // webpack@2.x and newer
+//                 },
+//               },
+//             ],
+//           },
+//           {
+//             test: /\.(js|jsx)$/,
+//             exclude: /node_modules/,
+//             loader: "babel-loader",
+//           },
+//         ]
+//       },
+      
+// }
+
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("./dist"),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
       },
-      output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-      devServer: {
-        hot: true,
-        contentBase: path.resolve("./dist"),
-        compress: true,
-        port: 3000,
-      },
-
-      module: {
-        rules: [
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
           {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
           },
-          {
-            test: /\.(gif|png|jpe?g|svg)$/i,
-            use: [
-              'file-loader',
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  bypassOnDebug: true, // webpack@1.x
-                  disable: true, // webpack@2.x and newer
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-          },
-        ]
+        ],
       },
-      
-}
-
+    ],
+  },
+};
