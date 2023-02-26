96f9654bd6c8b84cd636a2e9007ff0209a779fbf
anne <annemachinda@gmail.com>
2023-02-26 22:00:21 +0100

    fixed blank page error

--- 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItemShape.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItemShape.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 
-export default PropTypes.NotificationItemShape({
+export default PropTypes.shape({
     id: PropTypes.number.isRequired,
     html: PropTypes.shape({__html: PropTypes.string}),
     type: PropTypes.string.isRequired,
