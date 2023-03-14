ccb99d8765a359fd4e903035d4c4d90ca9ef88d7
anne <annemachinda@gmail.com>
2023-03-14 07:21:20 +0100

    seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,15 @@
 import { Seq } from 'immutable';
 
-export function printBestStudents(obj) {
+export default function printBestStudents(obj) {
   const seq = Seq(obj);
-  const students = seq.filter((val) => val.score > 70).toJS;
+  const students = seq.filter((val) => val.score > 70).toJS();
 
   Object.keys(students).map((key) => {
-    const first_name = students[key].firstName;
-    const last_name = students[key].lastName;
+    const fn = students[key].firstName;
+    const ln = students[key].lastName;
 
-    students[key].firstName = first_name.chartAt(0).toUpperCase() + first_name.slice(1);
-    students[key].lastName = last_name.chartAt(0).toUpperCase() + last_name.slice(1);
+    students[key].firstName = fn.charAt(0).toUpperCase() + fn.slice(1);
+    students[key].lastName = ln.charAt(0).toUpperCase() + ln.slice(1);
     return null;
   });
 
