f0bc37cb60d07e165bc33caa4475d7f632f60b74
anne <annemachinda@gmail.com>
2023-03-14 07:34:36 +0100

    lint

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -5,11 +5,11 @@ export default function printBestStudents(obj) {
   const students = seq.filter((val) => val.score > 70).toJS();
 
   Object.keys(students).map((key) => {
-    const first_name = students[key].firstName;
-    const last_name = students[key].lastName;
+    const firstname = students[key].firstName;
+    const lastname = students[key].lastName;
 
-    students[key].firstName = first_name.charAt(0).toUpperCase() + first_name.slice(1);
-    students[key].lastName = last_name.charAt(0).toUpperCase() + last_name.slice(1);
+    students[key].firstName = firstname.charAt(0).toUpperCase() + firstname.slice(1);
+    students[key].lastName = lastname.charAt(0).toUpperCase() + lastname.slice(1);
     return null;
   });
 
