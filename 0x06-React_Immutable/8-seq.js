e26383feeb0730d6ce755c1b3d263afdfcac9c6a
anne <annemachinda@gmail.com>
2023-03-14 07:14:19 +0100

    seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,17 @@
+import { Seq } from 'immutable';
+
+export function printBestStudents(obj) {
+  const seq = Seq(obj);
+  const students = seq.filter((val) => val.score > 70).toJS;
+
+  Object.keys(students).map((key) => {
+    const first_name = students[key].firstName;
+    const last_name = students[key].lastName;
+
+    students[key].firstName = first_name.chartAt(0).toUpperCase() + first_name.slice(1);
+    students[key].lastName = last_name.chartAt(0).toUpperCase() + last_name.slice(1);
+    return null;
+  });
+
+  console.log(students);
+}
