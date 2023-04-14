cd5a67f757891aa5717aba2c28825101a0845d00
Lukong Anne <annemachinda@gmail.com>
2023-04-14 11:43:55 +0100

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_8/dashboard/src/index.js
@@ -14,7 +14,8 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
+   
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 
