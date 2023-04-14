1c9f9f43bd269649a8ac454b937186b8012046af
Lukong Anne <annemachinda@gmail.com>
2023-04-14 11:40:54 +0100

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -5,12 +5,13 @@ import App from './App/App';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
 
-const store = createStore(uiReducer);
+const store =  createStore(uiReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+  
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
