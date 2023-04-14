7d3b4801f5c5076cffef44b76d04224cd32a2c71
Lukong Anne <annemachinda@gmail.com>
2023-04-14 10:13:27 +0100

    Update notifications.js

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,6 +1,7 @@
 import * as notifications from '../../../../notifications.json';
 
 export function getAllNotificationsByUser(userId) {
-  const selectedNotifications = notifications.default.filter((notif) => notif.author.id === userId);
+  const selectedNotifications = notifications.default.filter((notif) => notif.author.Id === userId);
   return selectedNotifications.map((notif => notif.context));
+
 }
