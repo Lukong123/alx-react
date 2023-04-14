eedcadf1fdb1a3361991a8768f48521655bad6cd
Lukong Anne <annemachinda@gmail.com>
2023-04-14 10:48:43 +0100

    Update uiReducer.test.js

--- 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
@@ -2,18 +2,18 @@ import { uiReducer, initialState } from './uiReducer';
 import * as actions from '../actions/uiActionTypes';
 
 describe('Testing uiReducer', () => {
-  it('verify the state returned by the uiReducer function equals the initial state when no action is passed', () => {
-    const res = uiReducer();
-    expect(res).toStrictEqual(initialState);
+  it('it verifies the state returned by the uiReducer function if it equals the initial state when no action is passed', () => {
+    const uiReducer = uiReducer();
+    expect(uiReducer).toStrictEqual(initialState);
   });
 
-  it('verify  the state returned by the uiReducer function equals the initial state when the action SELECT_COURSE is passed', () => {
-    const res = uiReducer(undefined, {type: "SELECT_COURSE"});
-    expect(res).toStrictEqual(initialState);
+  it('it verifies  the state returned by the uiReducer function equals the initial state when the action SELECT_COURSE is passed', () => {
+    const uiReducer = uiReducer(undefined, {type: "SELECT_COURSE"});
+    expect(uiReducer).toStrictEqual(initialState);
   });
 
-  it('verify the state returned by the uiReducer function, when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property', () => {
-    const res = uiReducer(undefined, {type: actions.DISPLAY_NOTIFICATION_DRAWER});
-    expect(res).toStrictEqual({ ...initialState, isNotificationDrawerVisible: true });
+  it('it verifies the state returned by the uiReducer function, when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property', () => {
+    const uiReducer = uiReducer(undefined, {type: actions.DISPLAY_NOTIFICATION_DRAWER});
+    expect(uiReducer).toStrictEqual({ ...initialState, isNotificationDrawerVisible: true });
   });
 });
