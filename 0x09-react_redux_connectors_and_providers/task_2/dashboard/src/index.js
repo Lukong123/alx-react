0ad695a0e7a7ee031c97733166716b724f54d87e
Lukong Anne <annemachinda@gmail.com>
2023-04-14 11:41:09 +0100

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -4,15 +4,14 @@ import { createStore } from 'redux';
 import App from './App/App';
 import { uiReducer } from './reducers/uiReducer';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extenstion';
 
-const store = createStore(uiReducer,  composeWithDevTools(applyMiddleware(thunk)));
+const store =  createStore(uiReducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+  
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
